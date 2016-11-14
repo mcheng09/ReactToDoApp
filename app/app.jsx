510e240a9842991746139e1f3787a14272a854b7
Michael Cheng <mcheng.oc@gmail.com>
2016-11-13 18:25:59 -0800

    Added redux stpre

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,17 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var ToDoApp = require('ToDoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New State', store.getState());
+});
+
+store.dispatch(actions.addToDo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted(''));
 // Load Foundation
 $(document).foundation();
 
