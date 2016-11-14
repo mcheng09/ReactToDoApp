0917777831015b5866be2fdbba1b7e4fcfec1ca0
Michael Cheng <mcheng.oc@gmail.com>
2016-11-13 18:09:55 -0800

    Added todosreducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,39 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('toDoReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.toDoReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+
+    });
+  });
+
+  describe('toggleToDoReducer', () => {
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '123',
+        text: 'Ohhhhhh',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+
+      var res = reducers.toDoReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
 });
